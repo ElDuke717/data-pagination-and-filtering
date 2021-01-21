63361747e6054ee861ced85156aad84f4db05f4a
Nick Huemmer <njhuemmer@gmail.com>
2021-01-21 11:59:10 -0800

    More updates to script.js.  Work on button active class

--- js/script.js
+++ js/script.js
@@ -22,15 +22,14 @@ function showPage(list, page){
    const endIndex = page * 9;
    
    // select the element with a class of `student-list` and assign it to a variable
-   const studentList = document.querySelector('ul');
+   const studentList = document.querySelector('.student-list');
    
     // set the innerHTML property of the variable you just created to an empty string
    studentList.innerHTML = "";
-
     // loop over the length of the `list` parameter
    for (let i = 0; i < list.length; i++) {
      // inside the loop create a conditional to display the proper students
-      if (endIndex > i >= startIndex) {
+      if ( i >= startIndex, i < endIndex) {
          // inside the conditional:
           // create the elements needed to display the student information
          const studentItem =  
@@ -47,9 +46,8 @@ function showPage(list, page){
          `
          studentList.insertAdjacentHTML("beforeend", studentItem);
          // insert the above elements
-         
       }
-   }
+   } 
 }
 
 
@@ -61,15 +59,25 @@ This function will create and insert/append the elements needed for the paginati
 
 function addPagination(list) {
    // create a variable to calculate the number of pages needed
- 
+   const numOfPages = Math.ceil(list.length / 9);  
    // select the element with a class of `link-list` and assign it to a variable
- 
+   const linkList = document.querySelector('.link-list');
    // set the innerHTML property of the variable you just created to an empty string
- 
+   linkList.innerHTML=""
    // loop over the number of pages needed
+   for (let i = 1; i <= numOfPages; i++) {
      // create the elements needed to display the pagination button
-     // insert the above elements
- 
+      const button = 
+      `<li>
+            <button type="button">${[i]}</button>
+      </li>`;
+      // insert the above elements
+      linkList.insertAdjacentHTML("beforeend", button);
+      document.querySelector('button').className = "active";
+   }
+   
+   console.log(document.querySelector('button'));
+   
    // give the first pagination button a class of "active"
  
    // create an event listener on the `link-list` element
@@ -83,4 +91,5 @@ function addPagination(list) {
 
 
 // Call functions 
-showPage(data, 1)
+showPage(data, 1);
+addPagination(data);
